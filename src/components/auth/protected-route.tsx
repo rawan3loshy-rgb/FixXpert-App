86b95d2dcca502fc458612ab2d5c43ed8a038643
Rami <raminaiim48@gmail.com>
2026-04-08 23:08:19 +0200

    fix

--- src/components/auth/protected-route.tsx
+++ src/components/auth/protected-route.tsx
@@ -2,20 +2,15 @@
 
 import { useEffect, useState } from "react"
 import { supabase } from "@/lib/supabase"
-import { usePathname } from "next/navigation"
+
 import { useRouter } from "next/navigation"
 
 export default function ProtectedRoute({ children }: any) {
 
   const [loading, setLoading] = useState(true)
   const router = useRouter()
-  const pathname = usePathname()
-   // 🔥 allow public routes
-  if (
-    pathname.startsWith("/track") ||
-    pathname.startsWith("/login") ||
-    pathname.startsWith("/signup")
-  )
+ 
+  
 
   useEffect(() => {
 
